10bfe86e1d42a6ef840e2e4d8f8c62e92c622777
Emmanuel Ihemegbulam <emmanuelroic@gmail.com>
2021-06-21 09:23:52 +0100

    deploy devops

--- src/src/db/config.js
+++ src/src/db/config.js
@@ -13,7 +13,14 @@ module.exports = {
     dialect: 'postgres'
   },
   production: {
-    use_env_variable: 'DB_PROD',
-    dialect: 'postgres'
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 }
